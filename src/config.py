b6f3eb9193574e78413da5521b79bd4eb83fbc56
mmadianos <mixalisspa@gmail.com>
2024-04-23 23:10:46 +0300

    Added impute and encoding choices to the preprocessor. Pipeline updated

--- src/config.py
+++ src/config.py
@@ -1,9 +1,12 @@
-{
-    "TRAINING_FILE": "../data/Championship.csv",
-    "MODEL_OUTPUT": "../models/scikit",
-    "MODEL": "DecisionClassifier",
-    "TEST_SIZE": 0.2,
-    "PARAMETERS": "None",
-    "PLOT_CONFUSION": "True",
-    "CV_STRATEGY": "StratifiedKFold"
+params = {
+    'TRAINING_FILE': '../data/Championship.csv',
+    'MODEL_OUTPUT': '../models/scikit',
+    'MODEL': 'DecisionClassifier',
+    'TEST_SIZE': 0.2,
+    'PARAMETERS': None,
+    'PLOT_CONFUSION': True,
+    'CV_STRATEGY': 'StratifiedKFold',
+    'SCALER_TYPE': 'Standard',
+    'ENCODER_TYPE': 'Label',
+    'IMPUTER_TYPE': None
 }
